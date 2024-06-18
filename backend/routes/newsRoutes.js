593749a22b1516ca52553884cbe1a758232a31c8
rohanv32 <rj2440@nyu.edu>
2024-06-18 15:56:55 +0400

    Update newsRoutes.js

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getNews } = require('../controllers/newsController');
-
 const router = express.Router();
+const { getNews, getJournalistRatings, storeJournalistRatings } = require('../controllers/newsController');
 
 router.get('/', getNews);
+router.get('/journalist/:journalist', getJournalistRatings);
+router.post('/journalist', storeJournalistRatings);
 
 module.exports = router;
